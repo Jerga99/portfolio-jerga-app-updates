4c88d31f0da82806186cf87e8dc5c40bda44319d
Filip Jerga <jerga99@gmail.com>
2021-03-24 09:38:41 +0000

    Reactstrap and bootstrap

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 
 import BaseLayout from "../components/layouts/BaseLayout";
+import { Container, Button } from "reactstrap";
 
 const Home = () => {
   return (
     <BaseLayout>
-      <h1>I am home page</h1>
+      <Container>
+        <Button color="danger">Danger</Button>
+      </Container>
     </BaseLayout >
   )
 }
