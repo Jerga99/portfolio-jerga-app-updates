35346a1f670774548fc34ca2b6ee4d8c6fd1edb5
Filip Jerga <jerga99@gmail.com>
2021-03-26 08:30:38 +0000

    Self typed

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -4,7 +4,6 @@ import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem
 } from 'reactstrap';
@@ -30,11 +29,11 @@ const Header = () => {
         color="transparent"
         dark
         expand="md">
-        <NavbarBrand>
+        <div className="navbar-brand">
           <Link href="/">
             <a className="port-navbar-brand">Filip Jerga</a>
           </Link>
-        </NavbarBrand>
+        </div>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import { Container, Row, Col } from 'reactstrap';
+import Typed from "react-typed";
 
+const roles = ["Developer", "Tech Lover", "Team Player", "React JS", "Angular"];
 class Index extends React.Component {
   render() {
     return (
@@ -37,6 +39,17 @@ class Index extends React.Component {
                     Get informed, collaborate and discover projects I was working on through the years!
                   </h1>
                 </div>
+                <Typed
+                  loop
+                  typeSpeed={70}
+                  backSpeed={70}
+                  strings={roles}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                />
                 <div className="hero-welcome-bio">
                   <h1>
                     Let's take a look on my work.
